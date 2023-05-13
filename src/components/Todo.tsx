409eb4e373b3884b7a1ed03c6b43e6b4dc332ce6
hussain-shariff <hussainowaiz070@gmail.com>
2023-05-13 21:35:16 +0530

    get todos

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,10 +1,14 @@
 import EditIcon from "@mui/icons-material/Edit"
 import DeleteIcon from "@mui/icons-material/Delete"
 
-export const Todo = () => {
+type TodoProps = {
+    name : string
+}
+
+export const Todo = ({name}: TodoProps) => {
 	return (
 		<div className=" flex items-center gap-3">
-			<h1 className=" font-bold text-2xl text-white">todo</h1>
+			<h1 className=" font-bold text-2xl text-white">{name}</h1>
 			<EditIcon color="secondary" />
 			<DeleteIcon color="secondary" />
 		</div>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,13 +1,22 @@
 import { Todo } from "./Todo"
+import { useQuery, useMutation, useQueryClient } from "react-query"
+import { getTodos, createTodo, updateTodo, deleteTodo } from "../api/todosApi"
 
 const TodoList = () => {
-  return (
-    <div className=" flex items-center flex-col gap-5">
-        <Todo/>
-        <Todo/>
-        <Todo/>
-    </div>
-  )
+	const { data, isLoading, isError, error } = useQuery("todos", getTodos)
+
+	if (isLoading) return <h1>Loading...</h1>
+
+	if (isError) return <h1>{error.message}</h1>
+
+	const todoElements = data.map((todo: any) => (
+		<Todo key={todo.id} name={todo.title} />
+	))
+	return (
+		<div className=" flex items-center flex-col gap-5">
+			{ todoElements }
+		</div>
+	)
 }
 
 export default TodoList
