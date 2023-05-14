34c2dce7f1cbf74653c485f8415b62c9fffb4dee
hussain-shariff <hussainowaiz070@gmail.com>
2023-05-14 12:13:11 +0530

    checkbox

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,6 @@
 import EditIcon from "@mui/icons-material/Edit"
 import DeleteIcon from "@mui/icons-material/Delete"
+import { Checkbox } from "@mui/material"
 
 type TodoProps = {
     name : string
@@ -8,6 +9,7 @@ type TodoProps = {
 export const Todo = ({name}: TodoProps) => {
 	return (
 		<div className=" flex items-center gap-3">
+			<Checkbox color="secondary" />
 			<h1 className=" font-bold text-2xl text-white">{name}</h1>
 			<EditIcon color="secondary" />
 			<DeleteIcon color="secondary" />
