f281dc9fcfeea4a4b56851f7c22f7509ad1ef27c
hussain-shariff <hussainowaiz070@gmail.com>
2023-05-14 15:14:33 +0530

    final commit

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,8 +1,11 @@
 import EditIcon from "@mui/icons-material/Edit"
 import DeleteIcon from "@mui/icons-material/Delete"
+import CheckIcon from "@mui/icons-material/Check"
 import { Checkbox, FormControlLabel } from "@mui/material"
 import { useMutation, useQueryClient } from "react-query"
 import { updateTodo, deleteTodo } from "../api/todosApi"
+import TextField from "@mui/material/TextField/TextField"
+import { useState, useRef } from "react"
 
 type TodoProps = {
 	todo: {
@@ -14,6 +17,9 @@ type TodoProps = {
 }
 
 export const Todo = ({ todo }: TodoProps) => {
+	const [isEdit, setisEdit] = useState(false)
+	const [editError, setEditError] = useState(false)
+	const editRef = useRef()
 	const queryClient = useQueryClient()
 	const deleteTodoMutation = useMutation(deleteTodo, {
 		onSuccess: () => {
@@ -26,22 +32,63 @@ export const Todo = ({ todo }: TodoProps) => {
 			queryClient.invalidateQueries("todos")
 		},
 	})
+	const handleEdit = () => {
+		if (editRef.current.value === "") {
+			setEditError(true)
+			return
+		}
+		updateTodoMutation.mutate({
+			...todo,
+			title: editRef.current.value,
+		})
+		setisEdit(false)
+		setEditError(false)
+	}
 	return (
 		<div className=" text-white flex items-center gap-3">
-			<FormControlLabel
-				control={
-					<Checkbox
-						color="secondary"
-						defaultChecked={todo.completed}
-						onChange={()=> updateTodoMutation.mutate({
-							...todo,
-							completed : !todo.completed
-						})}
-					/>
-				}
-				label={todo.title}
-			/>
-			<EditIcon color="secondary" className=" cursor-pointer" />
+			{!isEdit && (
+				<FormControlLabel
+					control={
+						<Checkbox
+							color="secondary"
+							defaultChecked={todo.completed}
+							onChange={() =>
+								updateTodoMutation.mutate({
+									...todo,
+									completed: !todo.completed,
+								})
+							}
+						/>
+					}
+					label={todo.title}
+				/>
+			)}
+			{isEdit && (
+				<TextField
+					focused
+					size="small"
+					error={editError}
+					helperText={editError ? "Write a Todo" : ""}
+					defaultValue={todo.title}
+					color="secondary"
+					InputProps={{ style: { color: "white" } }}
+					inputRef={editRef}
+				/>
+			)}
+			{!isEdit && (
+				<EditIcon
+					color="secondary"
+					className=" cursor-pointer"
+					onClick={() => setisEdit(true)}
+				/>
+			)}
+			{isEdit && (
+				<CheckIcon
+					color="secondary"
+					className=" cursor-pointer"
+					onClick={() => handleEdit()}
+				/>
+			)}
 			<DeleteIcon
 				color="secondary"
 				className=" cursor-pointer"
