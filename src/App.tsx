d777f687c432328b9459eb77ebf576dd34d1406b
hussain-shariff <hussainowaiz070@gmail.com>
2023-05-28 11:23:00 +0530

    adding apollo provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import TodoList from "./components/TodoList"
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
+
+const client = new ApolloClient({
+	uri: "http://localhost:5000/graphql",
+	cache: new InMemoryCache(),
+})
 
 function App() {
 	return (
-		<div className="min-h-screen bg-[#0b1829] py-20">
-			<TodoList />
-		</div>
+		<>
+			<ApolloProvider client={client}>
+				<div className="min-h-screen bg-[#0b1829] py-20">
+					<TodoList />
+				</div>
+			</ApolloProvider>
+		</>
 	)
 }
 
